5869b5cde8e2c1b663925e9aa2acc6e095c1c211
IAmDarquise <148552238+IAmDarquise@users.noreply.github.com>
2025-01-17 18:44:08 +0100

    i give up...... FOR TODAY

--- MyDragonMMO_main/Assets/Scripts/MovementNetworkController.cs
+++ MyDragonMMO_main/Assets/Scripts/MovementNetworkController.cs
@@ -7,247 +7,249 @@ using UnityEngine.Analytics;
 
 public class MovementNetworkController : NetworkBehaviour
 {
-    public NetworkVariable<Vector3> Position = new();
-    public NetworkVariable<Quaternion> Rotation = new();
-    public NetworkVariable<FixedString64Bytes> PlayerName = new();
-    public NetworkVariable<int> PlayerColourID = new();
-    public NetworkVariable<Color> PlayerColour = new();
+    //    public NetworkVariable<Vector3> Position = new();
+    //    public NetworkVariable<Quaternion> Rotation = new();
+    //    public NetworkVariable<FixedString64Bytes> PlayerName = new();
+    //    public NetworkVariable<int> PlayerColourID = new();
+    //    public NetworkVariable<Color> PlayerColour = new();
 
-    [SerializeField] private float thrustForce = 0;
-    [SerializeField] private float strafeForce = 0;
-    [SerializeField] private float verticalForce = 0;
-    [SerializeField] private float rotationSpeed = 0;
+    //    [SerializeField] private float thrustForce = 0;
+    //    [SerializeField] private float strafeForce = 0;
+    //    [SerializeField] private float verticalForce = 0;
+    //    [SerializeField] private float rotationSpeed = 0;
 
-    [SerializeField] private Rigidbody rb;
+    //    [SerializeField] private Rigidbody rb;
 
-    [SerializeField] private Material materialBlue;
-    [SerializeField] private Material materialOrange;
-    [SerializeField] private Material materialGreen;
-    [SerializeField] private Material materialCustom;
+    //    [SerializeField] private Material materialBlue;
+    //    [SerializeField] private Material materialOrange;
+    //    [SerializeField] private Material materialGreen;
+    //    [SerializeField] private Material materialCustom;
 
-    [SerializeField] private MeshRenderer[] playerMeshes;
+    //    [SerializeField] private MeshRenderer[] playerMeshes;
 
-    [SerializeField] private TextMeshPro playerName;
+    //    [SerializeField] private TextMeshPro playerName;
 
-    private int playerColourID;
+    //    private int playerColourID;
 
-    [SerializeField] private Camera cam;
-
-
-    [Rpc(SendTo.Server)]
-    void SubmitPositionRequestServerRpc(Vector3 position, Quaternion rotation, RpcParams rpcParams = default)
-    {
-        Position.Value = position;
-        Rotation.Value = rotation;
-
-    }
-
-    [Rpc(SendTo.Server)]
-    void SubmitPlayerInfoServerRpc(FixedString64Bytes name, int colourID, Color colour)
-    {
-        PlayerName.Value = name;
-        PlayerColourID.Value = colourID;
-        PlayerColour.Value = colour;
-    }
+    //    [SerializeField] private Camera cam;
 
 
-    private void Start()
-    {
-        SetRandomPlayerPosition();
+    //    [Rpc(SendTo.Server)]
+    //    void SubmitPositionRequestServerRpc(Vector3 position, Quaternion rotation, RpcParams rpcParams = default)
+    //    {
+    //        Position.Value = position;
+    //        Rotation.Value = rotation;
 
-        //if (IsOwner)
-        //{
-        //    PlayerName.Value = PlayerPrefs.GetString("Name");
-        //    PlayerColourID.Value = PlayerPrefs.GetInt("Colour");
-        //}
-        if (IsOwner)
-        {
-            PlayerColour.Value = materialCustom.color;
-        }
+    //    }
 
-        ApplyPlayerValues();
-    }
-
-    void ApplyPlayerValues()
-    {
-        cam = Camera.main;
-
-        playerMeshes = GetComponentsInChildren<MeshRenderer>();
-
-        playerName.text = PlayerName.Value.ToString();
-        playerColourID = PlayerColourID.Value;
-        //playerName.text = PlayerPrefs.GetString("Name");
-        //playerColourID = PlayerPrefs.GetInt("Colour");
-
-        Debug.Log(playerColourID);
-        switch(playerColourID)
-        {
-            case 0:
-                ChangeColour(PlayerColour.Value);
-                break;
-            case 1:
-                ChangeColour(materialBlue.color);
-                break;
-            case 2:
-                ChangeColour(materialOrange.color);
-                break;
-            case 3:
-                ChangeColour(materialGreen.color);
-                break;
-        }
-
-    }
-
-    void ChangeMaterial(Material mat)
-    {
-        foreach (MeshRenderer renderer in playerMeshes)
-        {
-            if (renderer.CompareTag("IgnoreMaterialChange"))
-            {
-                continue;
-            }
-
-            renderer.material = mat;
-            //renderer.material.color = PlayerColour.Value;
-        }
-    }
-    void ChangeColour(Color color)
-    {
-        foreach (MeshRenderer renderer in playerMeshes)
-        {
-            if (renderer.CompareTag("IgnoreMaterialChange"))
-            {
-                continue;
-            }
-
-            //renderer.material = mat;
-            renderer.material.color = color;
-        }
-    }
+    //    [Rpc(SendTo.Server)]
+    //    void SubmitPlayerInfoServerRpc(FixedString64Bytes name, int colourID, Color colour)
+    //    {
+    //        PlayerName.Value = name;
+    //        PlayerColourID.Value = colourID;
+    //        PlayerColour.Value = colour;
+    //    }
 
 
-    void Update()
-    {
-        
+    //    private void Start()
+    //    {
+    //        SetRandomPlayerPosition();
 
-        playerName.gameObject.transform.LookAt(cam.transform);
-        playerName.gameObject.transform.Rotate(0, 180, 0);
+    //        //if (IsOwner)
+    //        //{
+    //        //    PlayerName.Value = PlayerPrefs.GetString("Name");
+    //        //    PlayerColourID.Value = PlayerPrefs.GetInt("Colour");
+    //        //}
+    //        if (IsOwner && !IsServer)
+    //        {
+    //            PlayerColour.Value = materialCustom.color;
+    //        }
 
+    //        ApplyPlayerValues();
+    //    }
 
+    //    void ApplyPlayerValues()
+    //    {
+    //        cam = Camera.main;
 
-        if (IsOwner && !IsServer)
-        {
+    //        playerMeshes = GetComponentsInChildren<MeshRenderer>();
 
-            DoMovement();
-            DoRotation();
+    //        playerName.text = PlayerName.Value.ToString();
+    //        playerColourID = PlayerColourID.Value;
+    //        //playerName.text = PlayerPrefs.GetString("Name");
+    //        //playerColourID = PlayerPrefs.GetInt("Colour");
 
-            SubmitPositionRequestServerRpc(transform.position, transform.rotation);
-        }
+    //        Debug.Log(playerColourID);
+    //        switch(playerColourID)
+    //        {
+    //            case 0:
+    //                ChangeColour(PlayerColour.Value);
+    //                break;
+    //            case 1:
+    //                ChangeColour(materialBlue.color);
+    //                break;
+    //            case 2:
+    //                ChangeColour(materialOrange.color);
+    //                break;
+    //            case 3:
+    //                ChangeColour(materialGreen.color);
+    //                break;
+    //        }
 
+    //    }
 
-        if (IsServer)
-        {
-            transform.position = Position.Value;
-            transform.rotation = Rotation.Value;
+    //    void ChangeMaterial(Material mat)
+    //    {
+    //        foreach (MeshRenderer renderer in playerMeshes)
+    //        {
+    //            if (renderer.CompareTag("IgnoreMaterialChange"))
+    //            {
+    //                continue;
+    //            }
 
-        }
-        else if (!IsOwner)
-        {
-            transform.position = Position.Value;
-            transform.rotation = Rotation.Value;
-        }
+    //            renderer.material = mat;
+    //            //renderer.material.color = PlayerColour.Value;
+    //        }
+    //    }
+    //    void ChangeColour(Color color)
+    //    {
+    //        foreach (MeshRenderer renderer in playerMeshes)
+    //        {
+    //            if (renderer.CompareTag("IgnoreMaterialChange"))
+    //            {
+    //                continue;
+    //            }
 
-    }
-
-    void SetRandomPlayerPosition()
-    {
-        float xPos = Random.Range(-20.0f, 20.0f);
-        float zPos = Random.Range(-20.0f, 20.0f);
-        Vector3 newPosition = new Vector3(xPos, 10f, zPos);
-
-        if (IsOwner)
-        {
-            transform.position = newPosition;
-        }
-    }
-
-    void DoMovement()
-    {
-        float move = Input.GetAxis("Vertical");
-        rb.AddForce(transform.forward * move * thrustForce * Time.deltaTime, ForceMode.Force);
-
-        float strafe = Input.GetAxis("Horizontal");
-        rb.AddForce(transform.right * strafe * strafeForce * Time.deltaTime, ForceMode.Force);
-
-
-        if (Input.GetKey(KeyCode.Space))
-        {
-            rb.AddForce(transform.up * verticalForce * Time.deltaTime, ForceMode.Force);
-        }
-        else if (Input.GetKey(KeyCode.LeftShift))
-        {
-            rb.AddForce(-transform.up * verticalForce * Time.deltaTime, ForceMode.Force);
-        }
-
-    }
-
-    void DoRotation()
-    {
-        float pitch = 0f;
-        float yaw = 0f;
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            yaw = -1f;
-        }
-        else if(Input.GetKey(KeyCode.D))
-        {
-            yaw = 1f;
-        }
+    //            //renderer.material = mat;
+    //            renderer.material.color = color;
+    //        }
+    //    }
 
 
-        if (Input.GetKey(KeyCode.Space))
-        {
-            pitch = -1f;
-        }
-        else if (Input.GetKey(KeyCode.LeftShift))
-        {
-            pitch = 1f;
-        }
+    //    void Update()
+    //    {
 
-        rb.AddTorque(transform.up * yaw * rotationSpeed * Time.deltaTime, ForceMode.Force);
-        rb.AddTorque(transform.right * pitch * rotationSpeed * Time.deltaTime, ForceMode.Force);
 
-        Quaternion targetRotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y, 0f);
-        Quaternion currentRotation = transform.rotation;
+    //        playerName.gameObject.transform.LookAt(cam.transform);
+    //        playerName.gameObject.transform.Rotate(0, 180, 0);
 
-        rb.rotation = Quaternion.Slerp(currentRotation, targetRotation, Time.deltaTime * 2f);
 
-    }
 
-    public override void OnNetworkSpawn()
-    {
-        if (IsOwner)
-        {
-            SubmitPlayerInfoServerRpc(PlayerPrefs.GetString("Name"), PlayerPrefs.GetInt("Colour"), materialCustom.color);
-            cam.enabled = true;
-            cam.GetComponent<AudioListener>().enabled = true;
+    //        if (IsOwner && !IsServer)
+    //        {
 
-        }
-        else
-        {
-            cam.enabled = false;
-            cam.GetComponent<AudioListener>().enabled = false;
-        }
+    //            DoMovement();
+    //            DoRotation();
 
-        playerName.text = PlayerName.Value.ToString();
-        playerColourID = PlayerColourID.Value;
-        //ApplyPlayerValues();
+    //            SubmitPositionRequestServerRpc(transform.position, transform.rotation);
+    //        }
 
-        PlayerName.OnValueChanged += (oldValue, newValue) => { playerName.text = newValue.ToString(); };
-        PlayerColourID.OnValueChanged += (oldValue, newValue) => { playerColourID = newValue; };
-        PlayerColour.OnValueChanged += (oldValue, newValue) => { ChangeColour(newValue); };
-    }
+
+    //        if (IsServer)
+    //        {
+    //            transform.position = Position.Value;
+    //            transform.rotation = Rotation.Value;
+
+    //        }
+    //        else if (!IsOwner)
+    //        {
+    //            transform.position = Position.Value;
+    //            transform.rotation = Rotation.Value;
+    //        }
+
+    //    }
+
+    //    void SetRandomPlayerPosition()
+    //    {
+    //        float xPos = Random.Range(-20.0f, 20.0f);
+    //        float zPos = Random.Range(-20.0f, 20.0f);
+    //        Vector3 newPosition = new Vector3(xPos, 10f, zPos);
+
+    //        if (IsOwner)
+    //        {
+    //            transform.position = newPosition;
+    //        }
+    //    }
+
+    //    void DoMovement()
+    //    {
+    //        float move = Input.GetAxis("Vertical");
+    //        rb.AddForce(transform.forward * move * thrustForce * Time.deltaTime, ForceMode.Force);
+
+    //        float strafe = Input.GetAxis("Horizontal");
+    //        rb.AddForce(transform.right * strafe * strafeForce * Time.deltaTime, ForceMode.Force);
+
+
+    //        if (Input.GetKey(KeyCode.Space))
+    //        {
+    //            rb.AddForce(transform.up * verticalForce * Time.deltaTime, ForceMode.Force);
+    //        }
+    //        else if (Input.GetKey(KeyCode.LeftShift))
+    //        {
+    //            rb.AddForce(-transform.up * verticalForce * Time.deltaTime, ForceMode.Force);
+    //        }
+
+    //    }
+
+    //    void DoRotation()
+    //    {
+    //        float pitch = 0f;
+    //        float yaw = 0f;
+
+    //        if (Input.GetKey(KeyCode.A))
+    //        {
+    //            yaw = -1f;
+    //        }
+    //        else if(Input.GetKey(KeyCode.D))
+    //        {
+    //            yaw = 1f;
+    //        }
+
+
+    //        if (Input.GetKey(KeyCode.Space))
+    //        {
+    //            pitch = -1f;
+    //        }
+    //        else if (Input.GetKey(KeyCode.LeftShift))
+    //        {
+    //            pitch = 1f;
+    //        }
+
+    //        rb.AddTorque(transform.up * yaw * rotationSpeed * Time.deltaTime, ForceMode.Force);
+    //        rb.AddTorque(transform.right * pitch * rotationSpeed * Time.deltaTime, ForceMode.Force);
+
+    //        Quaternion targetRotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y, 0f);
+    //        Quaternion currentRotation = transform.rotation;
+
+    //        rb.rotation = Quaternion.Slerp(currentRotation, targetRotation, Time.deltaTime * 2f);
+
+    //    }
+
+    //    public override void OnNetworkSpawn()
+    //    {
+    //        Debug.Log("Player Spawn");
+
+    //        if (IsOwner)
+    //        {
+    //            SubmitPlayerInfoServerRpc(PlayerPrefs.GetString("Name"), PlayerPrefs.GetInt("Colour"), materialCustom.color);
+    //            cam.enabled = true;
+    //            cam.GetComponent<AudioListener>().enabled = true;
+
+    //        }
+    //        else
+    //        {
+    //            cam.enabled = false;
+    //            cam.GetComponent<AudioListener>().enabled = false;
+    //        }
+
+    //        playerName.text = PlayerName.Value.ToString();
+    //        playerColourID = PlayerColourID.Value;
+    //        //ApplyPlayerValues();
+
+    //        PlayerName.OnValueChanged += (oldValue, newValue) => { playerName.text = newValue.ToString(); };
+    //        PlayerColourID.OnValueChanged += (oldValue, newValue) => { playerColourID = newValue; };
+    //        PlayerColour.OnValueChanged += (oldValue, newValue) => { ChangeColour(newValue); };
+    //    }
 }
 
 
--- MyDragonMMO_main/Assets/Scripts/NetworkUptime.cs
+++ MyDragonMMO_main/Assets/Scripts/NetworkUptime.cs
@@ -5,89 +5,89 @@ using UnityEngine;
 using UnityEngine.Assertions;
 public class NetworkUptime : NetworkBehaviour
 {
-    private NetworkVariable<float> ServerUptimeNetworkVariable = new NetworkVariable<float>();
-    private float last_t = 0.0f;
+    //    private NetworkVariable<float> ServerUptimeNetworkVariable = new NetworkVariable<float>();
+    //    private float last_t = 0.0f;
 
 
-    [SerializeField] private TextMeshProUGUI uptimeText;
-    [SerializeField] private TextMeshProUGUI connectedClientsText;
-    [SerializeField] private TextMeshProUGUI deviceTypeText;
+    //    [SerializeField] private TextMeshProUGUI uptimeText;
+    //    [SerializeField] private TextMeshProUGUI connectedClientsText;
+    //    [SerializeField] private TextMeshProUGUI deviceTypeText;
 
-    [SerializeField] private Canvas canvas;
-    private bool isCanvasOn;
+    //    [SerializeField] private Canvas canvas;
+    //    private bool isCanvasOn;
 
-    private int connectedClientsAmount = 0;
-    private FixedString64Bytes deviceType = new FixedString64Bytes();
-
-
+    //    private int connectedClientsAmount = 0;
+    //    private FixedString64Bytes deviceType = new FixedString64Bytes();
 
 
-    private void Start()
-    {
-        Assert.IsNotNull(uptimeText);
-        Assert.IsNotNull(connectedClientsText);
-        Assert.IsNotNull(deviceTypeText);
 
 
-        canvas.enabled = false;
-        isCanvasOn = false;
-    }
-    public override void OnNetworkSpawn()
-    {
-        if (IsServer)
-        {
-            ServerUptimeNetworkVariable.Value = 0.0f;
-            Debug.Log("Server's uptime var initialized to: " + ServerUptimeNetworkVariable.Value);
-            deviceType = "Server";
-        }
-
-        if(IsClient)
-        {
-            deviceType = "Client";
-        }
-    }
+    //    private void Start()
+    //    {
+    //        Assert.IsNotNull(uptimeText);
+    //        Assert.IsNotNull(connectedClientsText);
+    //        Assert.IsNotNull(deviceTypeText);
 
 
-    void Update()
-    {
-        connectedClientsAmount = NetworkManager.Singleton.ConnectedClients.Count;
+    //        canvas.enabled = false;
+    //        isCanvasOn = false;
+    //    }
+    //    public override void OnNetworkSpawn()
+    //    {
+    //        if (IsServer)
+    //        {
+    //            ServerUptimeNetworkVariable.Value = 0.0f;
+    //            Debug.Log("Server's uptime var initialized to: " + ServerUptimeNetworkVariable.Value);
+    //            deviceType = "Server";
+    //        }
+
+    //        if(IsClient)
+    //        {
+    //            deviceType = "Client";
+    //        }
+    //    }
 
 
-        var t_now = Time.time;
-        if (IsServer)
-        {
-            ServerUptimeNetworkVariable.Value = ServerUptimeNetworkVariable.Value + 0.1f;
-            if (t_now - last_t > 0.5f)
-            {
-                last_t = t_now;
-                Debug.Log("Server uptime var has been updated to: " + ServerUptimeNetworkVariable.Value);
-
-                deviceTypeText.text = deviceType.ToString();
-                uptimeText.text = "Uptime: " + ServerUptimeNetworkVariable.Value.ToString();
-                connectedClientsText.text = "ConnectedClients: " + connectedClientsAmount.ToString();
-            }
-        }
+    //    void Update()
+    //    {
+    //        connectedClientsAmount = NetworkManager.Singleton.ConnectedClients.Count;
 
 
-        if (!IsServer)
-        {
-            deviceTypeText.text = deviceType.ToString();
-            uptimeText.text = "Uptime: " + ServerUptimeNetworkVariable.Value.ToString();
-            connectedClientsText.text = "ConnectedClients: " + connectedClientsAmount.ToString();
+    //        var t_now = Time.time;
+    //        if (IsServer)
+    //        {
+    //            ServerUptimeNetworkVariable.Value = ServerUptimeNetworkVariable.Value + 0.1f;
+    //            if (t_now - last_t > 0.5f)
+    //            {
+    //                last_t = t_now;
+    //                Debug.Log("Server uptime var has been updated to: " + ServerUptimeNetworkVariable.Value);
 
-        }
+    //                deviceTypeText.text = deviceType.ToString();
+    //                uptimeText.text = "Uptime: " + ServerUptimeNetworkVariable.Value.ToString();
+    //                connectedClientsText.text = "ConnectedClients: " + connectedClientsAmount.ToString();
+    //            }
+    //        }
 
-        if(Input.GetKey(KeyCode.F3) && !isCanvasOn)
-        {
-            canvas.enabled = true;
-            isCanvasOn = true;
-        }
-        else if (Input.GetKey(KeyCode.F3) && isCanvasOn)
-        {
-            canvas.enabled= false;
-            isCanvasOn = false;
-        }
-            
-            
-    }
+
+    //        if (!IsServer)
+    //        {
+    //            deviceTypeText.text = deviceType.ToString();
+    //            uptimeText.text = "Uptime: " + ServerUptimeNetworkVariable.Value.ToString();
+    //            connectedClientsText.text = "ConnectedClients: " + connectedClientsAmount.ToString();
+
+    //        }
+
+    //        if(Input.GetKey(KeyCode.F3) && !isCanvasOn)
+    //        {
+    //            canvas.enabled = true;
+    //            isCanvasOn = true;
+    //        }
+    //        else if (Input.GetKey(KeyCode.F3) && isCanvasOn)
+    //        {
+    //            canvas.enabled= false;
+    //            isCanvasOn = false;
+    //        }
+
+
+    //    }
 }
